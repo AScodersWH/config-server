147767f734272e90033392953085b3a1e3a45bab
AScodersWH <895052253@qq.com>
2018-08-29 17:19:10 +0800

    can be use

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 
 ./gradlew clean bootRepackage
 
-docker build --rm . --tag xueqingliao/config-server:${VER:?invalid version}
-docker push xueqingliao/config-server:${VER:?invalid version}
+docker build --rm . --tag as895052253/config-server:${VER:?invalid version}
+docker push as895052253/config-server:${VER:?invalid version}
 
 export VER
 docker stack deploy todo -c docker-compose.yml
